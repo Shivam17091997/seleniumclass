7573ae4a6cfa9e4386098633d5657216b3e243e4
Shivam Pathak <139621753+Shivam17091997@users.noreply.github.com>
2026-05-21 02:35:09 +0530

    Update TestRunner2.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/Stepdefinition/DynamicValues.java",
-        glue = "Stepdefinition"
+     //   features = "src/test/java/Stepdefinition/DynamicValues.java",
+      //  glue = "Stepdefinition"
       //  tags= "@smoke"
 )
 public class TestRunner {
